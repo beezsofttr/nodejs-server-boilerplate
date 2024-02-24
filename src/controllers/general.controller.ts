bc11f34149f3fd251a7c66467f21873f0121ea47
Günalp Uysal <gunalp@beezsoft.com>
2024-02-24 10:11:41 +0300

    add list customer

--- src/controllers/general.controller.ts
+++ src/controllers/general.controller.ts
@@ -1,16 +1,45 @@
 import { NextFunction, Request, Response } from 'express';
 import { HTTP_STATUS_CODE } from '../helpers';
+import { prisma } from '../libs';
 
 const checkVersionHandler = async (req: Request, res: Response, next: NextFunction) => {
-
   const currentVerison = '3.3.6';
 
   try {
     return res.status(HTTP_STATUS_CODE.OK).send(currentVerison);
-
   } catch (error) {
     next(error);
   }
 };
 
-export { checkVersionHandler };
+const listWithSelectedCustomerHandler = async (req: Request, res: Response, next: NextFunction) => {
+  const { selectedField } = req.params;
+
+  try {
+    const data = await prisma.customers.findMany({
+      where: {},
+      select: {
+        [selectedField]: true,
+      },
+    });
+
+    return res.status(HTTP_STATUS_CODE.OK).send(data);
+  } catch (error) {
+    next(error);
+  }
+};
+
+const listCustomerHandler = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const data = await prisma.customers.findMany({
+      where: {},
+      select: {},
+    });
+
+    return res.status(HTTP_STATUS_CODE.OK).send(data);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export { checkVersionHandler, listWithSelectedCustomerHandler, listCustomerHandler };
--- src/routes/general.route.ts
+++ src/routes/general.route.ts
@@ -1,8 +1,12 @@
 import { Router } from 'express';
-import {  checkVersionHandler } from '../controllers/general.controller';
+import { checkVersionHandler, listCustomerHandler, listWithSelectedCustomerHandler } from '../controllers/general.controller';
 
 const router = Router();
 
 router.all('/check-version', checkVersionHandler);
+
+router.all('/list-customers', listCustomerHandler);
+
+router.get('/list-customers/:selectedField', listWithSelectedCustomerHandler);
 
 export default router;
